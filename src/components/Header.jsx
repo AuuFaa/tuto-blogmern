3ed28f843fd2684befb0a87515ef7e93022cd5e8
iji <80830735+AuuFaa@users.noreply.github.com>
2023-12-02 20:55:23 +0800

    design transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { images } from '../constants';
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
 
 
 
 export default function Header() {
+  const [navIsVisible, setNavIsVisible] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) =>{
+      return !curState;
+    });
+  };
+
   return (
     <section>
-    <header className="container mx-auto px-5 py-3 bg-slate-600">
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      <div><img src={images.Logo} alt="logo" /></div>
-      <div className='flex gap-5 items-center'>
-      <ul className='flex gap-x-5 font-semibold'>
+    <header className="container mx-auto px-5 py-3 ">
+      <div className='flex justify-between  items-center  p-3'>
+      <div className="w-16"><img src={images.Logo} alt="logo" /></div>
+      <div className="lg:hidden z-50">
+        {navIsVisible ? <AiOutlineClose className="w-6 h-6" onClick={navVisibilityHandler}/> : <AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler}/>}
+      </div>
+      <div className={`${navIsVisible ? "right-0" : "-right-full"} transition duration-300 mt-[100px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-5 items-center `} >
+      <ul className='text-white lg:text-dark-soft flex flex-col lg:flex-row gap-x-5 font-semibold'>
           <li><a href="/">Home</a></li>
           <li><a href="/">Articles</a></li>
           <li><a href="/">Pages</a></li>
